1e6728b3936774a841d7eeb3865f388b63cc451f
shoutrix <dasshoutrik@gmail.com>
2025-04-29 16:55:04 +0530

    bug fixes

--- data_utils.py
+++ data_utils.py
@@ -5,7 +5,6 @@ import torch
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 
-    
 
 def preprocessor(train_data_path, valid_data_path):
     with open(train_data_path, "r", encoding="utf-8") as ft, open(valid_data_path, "r", encoding="utf-8") as fv:
@@ -15,7 +14,8 @@ def preprocessor(train_data_path, valid_data_path):
     train_source_chars = set("".join([l.strip().split(maxsplit=1)[0] for l in train_lines]))
     train_target_chars = set("".join([l.strip().split(maxsplit=1)[1] for l in train_lines]))
     valid_source_chars = set("".join([l.strip().split(maxsplit=1)[0] for l in valid_lines]))
-    valid_target_chars = set("".join([l.strip().split(maxsplit=1)[1] for l in train_lines]))
+    valid_target_chars = set("".join([l.strip().split(maxsplit=1)[1] for l in valid_lines]))
+
     
     source_chars = list(train_source_chars | valid_source_chars)
     target_chars = list(train_target_chars | valid_target_chars)
@@ -30,52 +30,69 @@ def preprocessor(train_data_path, valid_data_path):
         sv.write("\n".join(source_chars))
         tv.write("\n".join(target_chars))
         
+        
+class Lang:
+    def __init__(self, data_column, vocab_path):
+        with open(vocab_path, "r", encoding="utf-8") as v:
+            vocab_lines = v.read().splitlines()
+
+        self.stoi_dict = {s: i+1 for i, s in enumerate(vocab_lines)}
+        self.itos_dict = {v: k for k, v in self.stoi_dict.items()}
+
+        self.SOS = self.stoi_dict.get("<s>")
+        self.EOS = self.stoi_dict.get("</s>")
+        self.UNK = self.stoi_dict.get("<unk>")
+
+        self.data = list(data_column)
+        self.vocab_size = len(self.stoi_dict)+1
+
+    def stoi(self, str_: str):
+        return [self.stoi_dict.get(char, self.UNK) for char in str_]
+
+    def itos(self, idxs: torch.Tensor):
+        return "".join([self.itos_dict.get(idx.item(), "<unk>") for idx in idxs])
+
+    def __getitem__(self, idx):
+        word = self.data[idx]
+        word_tokenized = self.stoi(word)
+        return word_tokenized + [self.EOS]
+
+            
+        
 
 class TranslitDataset:
     def __init__(self, data_path):
         assert data_path.endswith(".tsv")
-        self.df = pd.read_csv(data_path, sep="\t", header=None, usecols=[0, 1], names=["source", "target"])
+        with open(data_path, "r", encoding="utf-8") as f:
+            lines = f.read().splitlines()
         
-        with open("dump/source_vocab.txt", "r", encoding="utf-8") as sv, open("dump/target_vocab.txt", "r", encoding="utf-8") as tv:
-            sv_lines = sv.read().splitlines()
-            tv_lines = tv.read().splitlines()
+        data = []
+        for line in lines:
+            parts = line.strip().split("\t")
+            if len(parts) >= 2:
+                data.append({"source": parts[0], "target": parts[1]})
         
-        self.source_stoi = {s:i+1 for i, s in enumerate(sv_lines)}
-        self.target_stoi = {s:i+1 for i, s in enumerate(tv_lines)}
-        self.source_itos = {v:k for k, v in self.source_stoi.items()}
-        self.target_itos = {v:k for k, v in self.target_stoi.items()}
+        self.df = pd.DataFrame(data)
 
-        
-    def stoi(self, str_: str, stoi_map: dict):
-        return [stoi_map.get(char, "<unk>") for char in str_]
-    
-    def itos(self, idxs: torch.Tensor, itos_map: dict):
-        return "".join([itos_map[idx.item()] for idx in idxs])
+        self.source = Lang(self.df["source"], "dump/source_vocab.txt")
+        self.target = Lang(self.df["target"], "dump/target_vocab.txt")
+
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-        s_idxs = self.stoi(self.df.iloc[idx]["source"], stoi_map=self.source_stoi) + [self.source_stoi["</s>"]]
-        t_idxs = self.stoi(self.df.iloc[idx]["target"], stoi_map=self.target_stoi) + [self.target_stoi["</s>"]]
+        s_idxs = self.source[idx]
+        t_idxs = self.target[idx]
         return torch.tensor(s_idxs, dtype=torch.long), torch.tensor(t_idxs, dtype=torch.long)
-    
-    
+
 
 def collate_fn(batch):
     source = [sample[0] for sample in batch]
     target = [sample[1] for sample in batch]
-    
+
     source = pad_sequence(source, batch_first=True, padding_value=0)
     target = pad_sequence(target, batch_first=True, padding_value=0)
-    
+
     return source, target
 
-    
-
-
-# for x, y in trainloader:
-#     print(x.shape, y.shape)
-#     print(x)
-#     print(y)
-#     break
--- translit_model.py
+++ translit_model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from dataclasses import dataclass
 from torch.utils.data import DataLoader
 from data_utils import preprocessor, TranslitDataset, collate_fn
+import torch.nn.functional as F
+from tqdm import tqdm
 
 @dataclass
 class TranslitModelConfig:
@@ -18,10 +20,11 @@ class TranslitModelConfig:
     decoder_bidirectional: bool = True
     dropout_p: float = 0.3
     max_length: int = 64
+    decoder_SOS: int = 0
 
     def __post_init__(self):
-        assert self.encoder_name in ["rnn", "lstm", "GRU"], f"Invalid encoder name: {self.encoder_name}. Must be 'rnn', 'lstm', or 'gru'."
-        assert self.decoder_name in ["rnn", "lstm", "GRU"], f"Invalid decoder name: {self.decoder_name}. Must be 'rnn', 'lstm', or 'gru'."
+        assert self.encoder_name in ["RNN", "GRU"], f"Invalid encoder name: {self.encoder_name}. Must be 'rnn', 'lstm', or 'gru'."
+        assert self.decoder_name in ["RNN", "GRU"], f"Invalid decoder name: {self.decoder_name}. Must be 'rnn', 'lstm', or 'gru'."
 
 
 class EncoderRNN(nn.Module):
@@ -49,8 +52,8 @@ class EncoderRNN(nn.Module):
         else:
             h_n = hidden
         return output, h_n
-
-
+    
+    
 class DecoderRNN(nn.Module):
     def __init__(self, args):
         super().__init__()
@@ -66,22 +69,43 @@ class DecoderRNN(nn.Module):
             dropout=args.dropout_p if args.decoder_num_layers > 1 else 0.0,
             bidirectional=args.decoder_bidirectional
         )
-        self.classifier_head = nn.Linear(args.hidden_size, args.target_vocab_size)
+        classifier_in_dim = args.hidden_size * (2 if args.decoder_bidirectional else 1)
+        self.classifier_head = nn.Linear(classifier_in_dim, args.target_vocab_size)
 
     def forward(self, encoder_outputs, encoder_hidden, target=None):
-        
-        
-        
-        
+        decoder_hidden = encoder_hidden
+        max_length = target.shape[1] if target is not None else self.args.max_length
+
+        decoder_input = torch.full((encoder_outputs.shape[0], 1), fill_value=self.args.decoder_SOS, dtype=torch.long, device=encoder_outputs.device)
+
         decoder_outputs = []
-        decoder_output, decoder_hidden = self.forward_one_step(decoder_input, decoder_hidden)
-        
-    
+        for i in range(max_length):
+            decoder_output, decoder_hidden = self.forward_one_step(decoder_input, decoder_hidden)
+            decoder_outputs.append(decoder_output)
+
+            if target is not None:
+                decoder_input = target[:, i].unsqueeze(1)
+            else:
+                probs = F.log_softmax(decoder_output, dim=-1)
+                decoder_input = torch.argmax(probs, dim=-1).detach()
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        return decoder_outputs
+
+
     def forward_one_step(self, decoder_input, decoder_hidden):
-        pass
-        
-        
-        
+        assert decoder_input.shape[1] == 1
+        embeds = self.relu(self.embedding(decoder_input))
+        embeds = self.dropout(embeds)
+        output, hidden = self.rnn(embeds, decoder_hidden)
+        if isinstance(hidden, tuple):
+            h_n, _ = hidden
+        else:
+            h_n = hidden
+        output = self.classifier_head(output)
+        return output, h_n
+
+
 
 
 class TranslitModel(nn.Module):
@@ -90,16 +114,31 @@ class TranslitModel(nn.Module):
         self.args = args
         self.encoder = EncoderRNN(args)
         self.decoder = DecoderRNN(args)
+        self.loss_fn = nn.CrossEntropyLoss()
+    
     
     def forward(self, source, target=None):
         encoder_outputs, encoder_hidden = self.encoder(source)
-        print("encoder_outputs : ", encoder_outputs.shape, encoder_hidden.shape)
-        loss, acc, decoder_outputs = self.decoder(encoder_outputs, encoder_hidden, target)
+        # print("encoder_outputs : ", encoder_outputs.shape, encoder_hidden.shape)
+        decoder_outputs = self.decoder(encoder_outputs, encoder_hidden, target)
+        
+        loss, acc = None, None
+        if target is not None:
+            loss, acc = self.compute_loss_and_acc(decoder_outputs, target)
         return loss, acc, decoder_outputs
+    
+    
+    def compute_loss_and_acc(self, decoder_outputs, target):
+        target_flattend = target.flatten()
+        _, _, n_classes = decoder_outputs.shape
+        deocder_outputs_flattened = decoder_outputs.view(-1, n_classes)
+        loss = self.loss_fn(deocder_outputs_flattened, target_flattend)
+        probs = F.log_softmax(deocder_outputs_flattened, dim=-1)
+        max_ = torch.argmax(probs, dim=-1)
+        acc = (max_ == target_flattend).sum() / len(target_flattend)
+        return loss, acc
 
 
-config = TranslitModelConfig()
-model = TranslitModel(config)
 
 train_data_path = "/speech/shoutrik/torch_exp/FliptyScripty/dakshina_dataset_v1.0/bn/lexicons/bn.translit.sampled.train.tsv"
 dev_data_path = "/speech/shoutrik/torch_exp/FliptyScripty/dakshina_dataset_v1.0/bn/lexicons/bn.translit.sampled.dev.tsv"
@@ -110,18 +149,45 @@ trainset = TranslitDataset(train_data_path)
 validset = TranslitDataset(dev_data_path)
 testset = TranslitDataset(test_data_path)
 
-trainloader = DataLoader(trainset, batch_size=4, collate_fn=collate_fn, shuffle=True)
 
+trainloader = DataLoader(trainset, batch_size=128, collate_fn=collate_fn, shuffle=True)
 
-for x, y in trainloader:
-    print(x.shape, y.shape)
-    # print(x)
-    # print(y)
-    out = model(x)
-    loss, acc, decoder_outputs = out
-    print(loss)
-    print(acc)
-    print(decoder_outputs.shape)
-    print("\n\n")
+config = TranslitModelConfig(
+    decoder_SOS=trainset.target.SOS,
+    source_vocab_size=trainset.source.vocab_size,
+    target_vocab_size=trainset.target.vocab_size
+    )
+
+device = "cuda"
+
+model = TranslitModel(config).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+max_epochs=10
+
+for epoch in range(max_epochs):
+    loss_track = 0
+    acc_track = 0
+    for x, y in tqdm(trainloader):
+        x = x.to(device)
+        y = y.to(device)
+        out = model(x, y)
+        loss, acc, decoder_outputs = out
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        loss_track += loss.item()
+        acc_track += acc.item()
+        
+    avg_loss = loss_track/len(trainloader)
+    avg_acc = acc_track/len(trainloader)
+
+    print(f"Epoch : {epoch+1} | Loss : {avg_loss:.4f} | Accuracy : {avg_acc:.4f}")
+        
+        
+        
+        
+
 
 
